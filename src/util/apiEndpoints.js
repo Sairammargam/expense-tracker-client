7db38bb1f6c4efff341f0d81bd18089f25606e9b
Sairammargam <sairam.margam2004@gmail.com>
2025-07-27 12:05:51 +0530

    added frontend-2

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+//export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL ="https://money-manager-u74q.onrender.com/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "dyugrhkfh";
 
 export const API_ENDPOINTS = {
